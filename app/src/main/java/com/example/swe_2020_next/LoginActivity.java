2615f06181d57f74548991dac7329a858debb0ea
Shrukhan199 <57919436+Shrukhan199@users.noreply.github.com>
2020-08-10 13:26:07 +0200

    Created View and Design for App Including navigation

---
+++ app/src/main/java/com/example/swe_2020_next/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.swe_2020_next;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void loginUser(View view) {
+        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+    }
+}
